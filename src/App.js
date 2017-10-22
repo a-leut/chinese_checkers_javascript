40884b1c1a56ef872b7b6749c964cbe4e154d410
aleut <turbomonkeyninja@gmail.com>
2017-10-22 12:59:49 -0600

    added grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Board from './Board.js'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>hi</h2>
+      <div class="container">
+        <Board knightPosition={[7, 4]} />,
       </div>
     );
   }
